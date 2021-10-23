c4b4e8fbc1f9f4b457810e72844ebd79814138d0
richlaw1986 <54599156+richlaw1986@users.noreply.github.com>
2021-10-24 00:43:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if select =='Keyword categoriser':
     keyword_file = st.file_uploader("Choose a CSV file", type='csv', key='4')
     if keyword_file is not None:
         st.write("Categorising...")
-        dffinal = querycat.pd.read_csv(keyword_file)
-        #catz = querycat.Categorize(dffinal, 'Keywords', min_support=3,  alg='apriori')
-        categories = dffinal.head()
+        dffinal = querycat.pd.read_csv(keyword_file, index=0)
+        catz = querycat.Categorize(dffinal, 'Keywords', min_support=3,  alg='apriori')
+        categories = querycat.dffinal.head()
         st.write(categories)
