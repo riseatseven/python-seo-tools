9fd7f496da042fb8c037a8a7563ac7569677ff50
richlaw1986 <54599156+richlaw1986@users.noreply.github.com>
2022-01-06 17:27:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -345,6 +345,10 @@ if select =='Search volume gatherer':
     volumes_file = st.file_uploader("Choose a CSV file", type='csv', key='14')
     if volumes_file is not None:
         volumes_file = pd.read_csv(volumes_file)
+        volumes_file['keywords'].str.strip()
+        volumes_file['length'] = volumes_file.keywords.str.len()
+        volumes_file = volumes_file[volumes_file.length < 80]
+        volumes_file = volumes_file.drop(columns=['length'])
         df_list = volumes_file['keywords'].tolist()
         if locations_file is not None:
             if select =='All 50 US states':
